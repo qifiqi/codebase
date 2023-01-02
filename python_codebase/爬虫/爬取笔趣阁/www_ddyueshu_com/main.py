bf91a2a30bb2c1ad2de95ab11c213e35c7afb51f
fuqing <2737454073@qq.com>
2023-01-02 23:55:41 +0800

    xiaoshuo

--- python_codebase/爬虫/爬取笔趣阁/www_ddyueshu_com/main.py
+++ python_codebase/爬虫/爬取笔趣阁/www_ddyueshu_com/main.py
@@ -1,16 +1,14 @@
-import hashlib
 import os
 import time
 
-from aligo import Aligo
 import requests
 from concurrent.futures import ThreadPoolExecutor
 import queue
 from tqdm import tqdm
-from lxml import etree
+from parsel import Selector
 
 """
-http://www.jianlaixiaoshuo.com/
+https://www.ddyueshu.com/
 """
 
 q = queue.Queue()
@@ -18,36 +16,30 @@ th = ThreadPoolExecutor(max_workers=32)
 
 head = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.33",
-    "Referer": "https://www.tyxsw.org/read/38590/"
+    "Referer": "https://www.ddyueshu.com/12_12497/"
 }
 
-titles = "剑来"
+titles = "我真没想重生啊"
 titles_text = '正文'
 
 
 def put_list(url: str):
     head['Referer'] = url
     response = requests.get(url=url, headers=head)
-    html = etree.HTML(response.content.decode("utf-8"))
+    html = Selector(response.content.decode("gbk"))
 
     global titles
     global titles_text
-    titles_sum = html.xpath("/html/body/div[1]/div/dl/dt[1]/text()")[0]
-    titles_text = html.xpath("/html/body/div[1]/div/dl/dt[2]/text()")[0]
-    titles = html.xpath("/html/body/div[1]/div/div/div/h1/a/text()")[0]
+    titles_text = html.css("dl dt:nth-child(8)::text").get("")
+    titles = html.css("#info h1::text").get("")
 
     if not os.path.exists(f'./{titles}'):
-        os.makedirs(f"./{titles}/{titles_sum}/")
         os.makedirs(f"./{titles}/{titles_text}/")
 
     dd_list = html.xpath("//dl/dd")
 
-    for dd in dd_list[0:48]:
-        path = "http://www.jianlaixiaoshuo.com" + dd.xpath('./a/@href')[0]
-        get_text(None, path, titles_sum)
-
-    for i, dd in enumerate(dd_list[48:]):
-        path = "http://www.jianlaixiaoshuo.com" + dd.xpath('./a/@href')[0]
+    for i, dd in enumerate(dd_list[6:]):
+        path = "https://www.ddyueshu.com" + dd.xpath('./a/@href').get("")
         q.put((i, path, titles_text))
 
     pass
@@ -55,20 +47,30 @@ def put_list(url: str):
 
 def get_text(i, path, section):
     response = requests.get(url=path, headers=head)
-    html = etree.HTML(response.content.decode("utf-8"))
-    title = html.xpath('//*[@id="BookCon"]/h1/text()')[0]
-    text_list = html.xpath('//div[@id="BookText"]//text()')
+    html = Selector(response.content.decode("gbk"))
+    title = html.css(".bookname h1::text").get("")
+    text_list = html.xpath('//div[@id="content"]//text()').getall()
     print(f"{title} 写入文件")
     if i is not None:
-        with open(f'./{titles}/{section}/{i}_{title}.txt', 'w+', encoding='utf-8') as files:
+        with open(f'./{titles}/{section}/{i}_{title.strip()}.txt', 'w+', encoding='utf-8') as files:
             files.write(title + '\r\n')
-            for text in text_list[1:-1]:
-                files.write(text.replace("$nbsp;", ''))
+            for text in text_list[:-2]:
+                if len(text) < 2:
+                    continue
+                files.write(text.replace("\xa0", '').replace("\r", '')+"\n")
+            else:
+                files.write("\r\n")
+
     else:
         with open(f'./{titles}/{section}/{title}.txt', 'w+', encoding='utf-8') as files:
             files.write(title + '\r\n')
-            for text in text_list[1:-1]:
-                files.write(text.replace("$nbsp;", ''))
+            for text in text_list[:-2]:
+                if len(text) < 2:
+                    continue
+                files.write(text.replace("\xa0", '').replace("\r", '')+"\n")
+            else:
+                files.write("\r\n")
+
     print(f"{title} 写入完成")
 
 
@@ -95,20 +97,14 @@ def main(url):
     time.sleep(32)
 
 
-def get_ali():
-    ali = Aligo()
-
-    ali.sync_folder(
-        local_folder=f"./{titles}",
-        remote_folder="62d80bbe9100e320545e48e0a7d918cf802e57f8"
-    )
-
 def run():
-    url_path = "http://www.jianlaixiaoshuo.com/"
+    url_path = "https://www.ddyueshu.com/12_12497/"
+    # put_list(url_path)
+    # get_text(1, "https://www.ddyueshu.com/12_12497/5886238.html", "《我真没想重生啊》正文")
+
     main(url_path)
     th.shutdown(wait=True)
     sum_text()
-    get_ali()
 
 
 if __name__ == '__main__':

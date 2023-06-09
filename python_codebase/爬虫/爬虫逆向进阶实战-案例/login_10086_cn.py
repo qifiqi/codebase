17f817ca75b66e30f99241c5086baad394f77e17
fuqing <2737454073@qq.com>
2023-06-09 23:59:30 +0800

    2023-6-9

--- python_codebase/爬虫/爬虫逆向进阶实战-案例/login_10086_cn.py
+++ python_codebase/爬虫/爬虫逆向进阶实战-案例/login_10086_cn.py
@@ -1,9 +1,17 @@
+import base64
+import time
+
 import requests
-from
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
 
-def rsa_decode():
-    pass
 
+def rsa_decode(data):
+    key = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsgDq4OqxuEisnk2F0EJFmw4xKa5IrcqEYHvqxPs2CHEg2kolhfWA2SjNuGAHxyDDE5MLtOvzuXjBx/5YJtc9zj2xR/0moesS+Vi/xtG1tkVaTCba+TV+Y5C61iyr3FGqr+KOD4/XECu0Xky1W9ZmmaFADmZi7+6gO9wjgVpU9aLcBcw/loHOeJrCqjp7pA98hRJRY+MML8MK15mnC4ebooOva+mJlstW6t/1lghR8WNV8cocxgcHHuXBxgns2MlACQbSdJ8c6Z3RQeRZBzyjfey6JCCfbEKouVrWIUuPphBL3OANfgp0B+QG31bapvePTfXU48TYK0M5kE+8LgbbWQIDAQAB'
+    rsa_key = RSA.import_key(base64.b64decode(key))
+    cipher = PKCS1_v1_5.new(rsa_key)
+    cipher_text = base64.b64encode(cipher.encrypt(data.encode(encoding="utf-8")))
+    return cipher_text.decode("utf-8")
 
 
 headers = {
@@ -23,22 +31,66 @@ headers = {
     "sec-ch-ua-platform": "^\\^Android^^"
 }
 cookies = {
-    "sendflag": "20230609140557174860",
-    "CaptchaCode": "BFNoRy"
+    "sendflag": "20230609163511711776",
+    "CaptchaCode": "BuNZBB",
+    "rdmdmd5": "63C7A1126B4CEB07EE780791556FAADA"
 }
+aa_time = str(int(time.time() * 1000))
+with open("aa.png", "wb+") as f:
+    aa = requests.get(f'https://login.10086.cn/captchazh.htm?type=03&timestamp={aa_time}',
+                      headers={
+                          "GET": "/captchazh.htm?type=03&timestamp=1686300262296 HTTP/1.1",
+                          "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+                          "Accept-Encoding": "gzip, deflate, br",
+                          "Accept-Language": "zh-CN,zh;q=0.9",
+                          "Connection": "keep-alive",
+                          "Cookie": "sendflag=20230609163511711776; CaptchaCode=IGQyXY",
+                          "Host": "login.10086.cn",
+                          "Referer": "https://login.10086.cn/html/login/email_login.html",
+                          "Sec-Fetch-Dest": "image",
+                          "Sec-Fetch-Mode": "no-cors",
+                          "Sec-Fetch-Site": "same-origin",
+                          "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+                          "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+                          "sec-ch-ua-mobile": "?1",
+                          "sec-ch-ua-platform": "\"Android\""
+                      }).content
+    f.write(aa)
+code = input("")
+for i in range(len(code)+1):
+    print(code[0:i])
+    requests.get(f'https://login.10086.cn/verifyCaptcha?inputCode={code[0:i]}', headers={
+        "GET": "/verifyCaptcha?inputCode=11 HTTP/1.1",
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "zh-CN,zh;q=0.9",
+        "Connection": "keep-alive",
+        "Cookie": "sendflag=20230609163511711776; CaptchaCode=BuNZBB; rdmdmd5=63C7A1126B4CEB07EE780791556FAADA",
+        "Host": "login.10086.cn",
+        "Referer": "https://login.10086.cn/html/login/email_login.html",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+        "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": "\"Android\""
+    })
+
 url = "https://login.10086.cn/login.htm"
 data = {
     "accountType": "02",
     "pwdType": "03",
-    "account": "hgtmS4iSwNejGGgK2a1LdzI^%^2Bvw6fkjRp5obWuOK5nwf4U71^%^2F^%^2BEtHRc428fRS21eJz7a^%^2BQxgZ3xJwT1b1LTmavC0gzDiJEx6Qz882ML0Yr6^%^2BdUh0EuAjGaKbVHVRxACd67TypGgdfxs^%^2FgWeOxi^%^2FUDT4ynkDhUwFPzitfdC7LtHGPKeJmhd^%^2B7ggAznm4nn01MaY^%^2B43BgOrDoFtrBQs99^%^2FVsoUA5EujTDVQoN9TJML1epJoMwkGJ1b2x4kXJx6tmKPJKTqwQ1PdrghIoxsLki2wYqeQJ23gkJ2QkDVlsuIJK50uPuacT5iYi7Jl0dsTg^%^2FGDzxGoDG1Uhe961YEgAZEI4w^%^3D^%^3D",
-    "password": "OP^%^2BcBxQTB1^%^2BEHpbvuphcMIl2s^%^2BpvokmOq6y30ZsZVkdK8xjMz6D2xuWTHNpVGDL6CiW9pSGWkUFp08zvJ4R2EE^%^2BthfbzRlqNWVH199oCy83UPG7hSORqJFyflh0Ne6yxocbkniRKzgORcc9rhNmOONTwnJ3YbGIQzA84eKImg9xHceBarombLlVl5FzbBk4Um^%^2FxNcn^%^2FpwlKvHXp2bJoMfsx6d0OFv5Igc4LenJHKONxFX9tyrK8nK6fMMQXRszwmTxi1U20wQAj8C8pozT^%^2BoydnTdTAY92X7w^%^2Fb3uLyjIpFxbszikgE3GcTATZBXGoz0Xn70UK9Gatypxx4lasM4Ag^%^3D^%^3D",
-    "inputCode": "77",
-    "backUrl": "https^%^3A^%^2F^%^2Ftouch.10086.cn^%^2Fi^%^2F",
+    "account": rsa_decode("2737454073@qq.com"),
+    "password": rsa_decode("123456"),
+    "inputCode": code,
+    "backUrl": "https://touch.10086.cn/i/",
     "rememberMe": "0",
     "channelID": "12014",
-    "protocol": "https^%^3A",
+    "protocol": "https:",
     "loginMode": "03",
-    "timestamp": "1686291261811"
+    "timestamp": aa_time
 }
 response = requests.post(url, headers=headers, cookies=cookies, data=data)
 
